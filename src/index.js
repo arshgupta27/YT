3fca34f96c618bd01b8e5fe63d69cbdfb8816cf3
arshgupta27 <129968842+arshgupta27@users.noreply.github.com>
2024-09-08 22:40:01 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./.env"
 })
 
-connectDB();
+connectDB()
+  .then(()=>{
+    console.log("Database Connected Successfully");
+    app.listen(process.env.PORT || 8000, ()=>{
+      app.on("error", (error)=>{
+        console.log("Error in intitalizing the app:", error);
+        throw error;
+      })
+      console.log("Server is running at :", process.env.PORT || 8000);
+    })
+  })
+  .catch((error)=>{
+    console.log("Database connection failed: ", error);
+  })
---
+++ src/utils/APIError.js
@@ -0,0 +1,24 @@
+class APIError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.errors = errors;
+    this.message = message;
+    this.stack = stack;
+    this.success = false;
+    if(stack){
+      this.stack = stack;
+    }
+    else{
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { APIError };
